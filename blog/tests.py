47dd529cd5c6ccda9f6a800e45fa2e248b290fbb
ercmilla <ercmilla@gmail.com>
2015-03-26 13:51:25 -0400

    added functional tests

--- blog/tests.py
+++ blog/tests.py
@@ -6,20 +6,24 @@ from pyramid import testing
 from .models import DBSession
 
 
+def _initTestingDB():
+    from sqlalchemy import create_engine
+    from .models import Base, Blog
+
+    engine = create_engine('sqlite://')
+    DBSession.configure(bind=engine)
+    Base.metadata.create_all(engine)
+    with transaction.manager:
+        model = Blog(title='testing title', body='testing body')
+        DBSession.add(model)
+    return DBSession
+
+
 class TestBlogPages(unittest.TestCase):
     def setUp(self):
-        from sqlalchemy import create_engine
-        from .models import Base, Blog
-
+        self.session = _initTestingDB()
         request = testing.DummyRequest()
         self.config = testing.setUp(request=request)
-
-        engine = create_engine('sqlite://')
-        DBSession.configure(bind=engine)
-        Base.metadata.create_all(engine)
-        with transaction.manager:
-            model = Blog(title='testing title', body='testing body')
-            DBSession.add(model)
 
     def tearDown(self):
         DBSession.remove()
@@ -70,3 +74,28 @@ class TestBlogPages(unittest.TestCase):
         query = DBSession.query(Blog).filter_by(id=request.matchdict['id']).first()
         self.assertIn('Successfully Deleted', data.body)
         self.assertEqual(query, None)
+
+
+class FunctionalTests(unittest.TestCase):
+    def setUp(self):
+        from blog import main
+        settings = {'sqlalchemy.url': 'sqlite://'}
+        app = main({}, **settings)
+        from webtest import TestApp
+        self.testapp = TestApp(app)
+        _initTestingDB()
+
+    def tearDown(self):
+        del self.testapp
+        from .models import DBSession
+        DBSession.remove()
+
+    def test_root(self):
+        res = self.testapp.get('/blog/', status=200)
+        self.assertEqual(res.content_type, 'application/json')
+        self.assertIn('[{', res.body)
+
+    def test_entry(self):
+        res = self.testapp.get('/blog/1', status=200)
+        self.assertEqual(res.content_type, 'application/json')
+        self.assertIn('[{', res.body)
